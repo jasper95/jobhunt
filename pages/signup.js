04e4a5c4b235d7ae79899eafc506f7bbd7ceb2a6
Jasper Bernales <bernalesjasper@gmail.com>
2019-04-23 00:38:55 +0800

    add page layout signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import Page from 'components/Layout/Page'
 import Button from '@material-ui/core/Button';
 import Link from 'next/link';
 import TextField from '@material-ui/core/TextField';
@@ -33,99 +35,101 @@ function SignupPage(props){
   } = formHandlers
   const { fields, errors } = formState
   return (
-    <div style={{ textAlign: 'center', margin: '0 20px' }}>
-      <Button onClick={() => onChange('role', 'USER')} children='Candidate' />
-      <Button onClick={() => onChange('role', 'ADMIN')} children='Employer' />
-      <form noValidate autoComplete='off'>
-        {fields.role === 'ADMIN' ? (
-          <>
-            <TextField
-              id='company_name'
-              label='Company Name'
-              type='company_name'
-              margin='normal'
-              variant='outlined'
-              onChange={onElementChange}
-              value={fields.company_name}
-              error={!!errors.company_name}
-              helperText={errors.company_name}
-            />
-          </>
-        ) : (
-          <>
-            <TextField
-              id='first_name'
-              label='First Name'
-              type='first_name'
-              margin='normal'
-              variant='outlined'
-              onChange={onElementChange}
-              value={fields.first_name}
-              error={!!errors.first_name}
-              helperText={errors.first_name}
-            />
-            <TextField
-              id='last_name'
-              label='Last Name'
-              type='last_name'
-              margin='normal'
-              variant='outlined'
-              onChange={onElementChange}
-              value={fields.last_name}
-              error={!!errors.last_name}
-              helperText={errors.last_name}
-            />
-          </>
-        )}
-        <br/>
-        <TextField
-          id='email'
-          label='Email'
-          type='email'
-          margin='normal'
-          variant='outlined'
-          onChange={onElementChange}
-          value={fields.email}
-          error={!!errors.email}
-          helperText={errors.email}
-        />
-        <br/>
-        <TextField
-          id='password'
-          variant='outlined'
-          type={fields.isShowPassword ? 'text': 'password' }
-          label='Password'
-          value={fields.password}
-          error={!!errors.password}
-          helperText={errors.password}
-          onChange={onElementChange}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position='end'>
-                <IconButton
-                  aria-label='Toggle password visibility'
-                  onClick={() => onChange('isShowPassword', !fields.isShowPassword)}
-                >
-                  {fields.isShowPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <br/>
-        <Button
-          variant='contained'
-          color='primary'
-          style={styleLoginButton}
-          onClick={onValidate}
-          children='Signup'
-        />
-        <span>Already have an account?</span>
-        <Link href="/login">
-          <a>Login Now</a>
-        </Link>
-      </form>
-    </div>
+    <Page>
+      <div style={{ textAlign: 'center', margin: '0 20px' }}>
+        <Button onClick={() => onChange('role', 'USER')} children='Candidate' />
+        <Button onClick={() => onChange('role', 'ADMIN')} children='Employer' />
+        <form noValidate autoComplete='off'>
+          {fields.role === 'ADMIN' ? (
+            <>
+              <TextField
+                id='company_name'
+                label='Company Name'
+                type='company_name'
+                margin='normal'
+                variant='outlined'
+                onChange={onElementChange}
+                value={fields.company_name}
+                error={!!errors.company_name}
+                helperText={errors.company_name}
+              />
+            </>
+          ) : (
+            <>
+              <TextField
+                id='first_name'
+                label='First Name'
+                type='first_name'
+                margin='normal'
+                variant='outlined'
+                onChange={onElementChange}
+                value={fields.first_name}
+                error={!!errors.first_name}
+                helperText={errors.first_name}
+              />
+              <TextField
+                id='last_name'
+                label='Last Name'
+                type='last_name'
+                margin='normal'
+                variant='outlined'
+                onChange={onElementChange}
+                value={fields.last_name}
+                error={!!errors.last_name}
+                helperText={errors.last_name}
+              />
+            </>
+          )}
+          <br/>
+          <TextField
+            id='email'
+            label='Email'
+            type='email'
+            margin='normal'
+            variant='outlined'
+            onChange={onElementChange}
+            value={fields.email}
+            error={!!errors.email}
+            helperText={errors.email}
+          />
+          <br/>
+          <TextField
+            id='password'
+            variant='outlined'
+            type={fields.isShowPassword ? 'text': 'password' }
+            label='Password'
+            value={fields.password}
+            error={!!errors.password}
+            helperText={errors.password}
+            onChange={onElementChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='Toggle password visibility'
+                    onClick={() => onChange('isShowPassword', !fields.isShowPassword)}
+                  >
+                    {fields.isShowPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <br/>
+          <Button
+            variant='contained'
+            color='primary'
+            style={styleLoginButton}
+            onClick={onValidate}
+            children='Signup'
+          />
+          <span>Already have an account?</span>
+          <Link href="/login">
+            <a>Login Now</a>
+          </Link>
+        </form>
+      </div>
+    </Page>
   )
 
   function onValid(data) {
